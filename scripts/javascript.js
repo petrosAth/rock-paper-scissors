b7f0ad1e2fc987a2f92091566333b9d9e956dac2
petrosAth <develop@athanasoulis.me>
2022-09-30 14:32:36 +0300

    Add a simple UI

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,95 +1,155 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function isGameOver() {
+  if (playerScore >= 5 || computerScore >= 5) {
+    return true;
+  }
+  return false;
+}
+
 function getComputerChoice() {
-    const num = Math.floor(Math.random() * 3) + 1;
-    switch (num) {
-        case 1:
-            return "Rock";
-        case 2:
-            return "Paper";
-        case 3:
-            return "Scissors";
-    }
+  const num = Math.floor(Math.random() * 3) + 1;
+  switch (num) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
 }
 
-function getPlayerChoice() {
-    let userInput = prompt(
-        "Choose 'Rock', 'Paper' or 'Scissors': "
-    ).toLowerCase();
+function showWinMsg(element, winner) {
+  const playerWinMsg = "You Won!";
+  const computerWinMsg = "Computer Won!";
+  const tieMsg = "Tie!";
 
-    let capitalizeFirstLetter = userInput.charAt(0).toUpperCase();
-    const playerSelection = capitalizeFirstLetter + userInput.slice(1);
-
-    return playerSelection;
+  if (winner === "player") {
+    element.textContent = playerWinMsg;
+  } else if (winner === "computer") {
+    element.textContent = computerWinMsg;
+  } else {
+    element.textContent = tieMsg;
+  }
 }
 
-function playRound(playerSelection, computerSelection, score) {
-    const winMsg = `You Win! ${playerSelection} wins ${computerSelection}`;
-    const loseMsg = `You Lose! ${computerSelection} wins ${playerSelection}`;
-    const tieMsg = `Tie! You both picked ${playerSelection}`;
+function pickIcon(selection) {
+  switch (selection) {
+    case "Rock":
+      return "fa-regular fa-hand-back-fist";
+    case "Paper":
+      return "fa-regular fa-hand";
+    case "Scissors":
+      return "fa-regular fa-hand-scissors";
 
-    switch (computerSelection) {
-        case "Rock":
-            if (playerSelection === computerSelection) {
-                score.player++;
-                score.computer++;
-                return tieMsg;
-            } else if (playerSelection === "Paper") {
-                score.player++;
-                return winMsg;
-            } else {
-                score.computer++;
-                return loseMsg;
-            }
-        case "Paper":
-            if (playerSelection === computerSelection) {
-                score.player++;
-                score.computer++;
-                return tieMsg;
-            } else if (playerSelection === "Scissors") {
-                score.player++;
-                return winMsg;
-            } else {
-                score.computer++;
-                return loseMsg;
-            }
-        case "Scissors":
-            if (playerSelection === computerSelection) {
-                score.player++;
-                score.computer++;
-                return tieMsg;
-            } else if (playerSelection === "Rock") {
-                score.player++;
-                return winMsg;
-            } else {
-                score.computer++;
-                return loseMsg;
-            }
-        default:
-            return "Invalid selection!";
-    }
+    default:
+      return "fa-solid fa-minus";
+  }
 }
 
-function game() {
-    let resultStatement = "";
-    const score = {
-        player: 0,
-        computer: 0,
-    };
+function showResults(playerSelection, computerSelection) {
+  let playerPickIcon = document.querySelector("#playerPickIcon");
+  let computerPickIcon = document.querySelector("#computerPickIcon");
+  let playerScoreValue = document.querySelector("#playerScoreValue");
+  let computerScoreValue = document.querySelector("#computerScoreValue");
 
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(getPlayerChoice(), getComputerChoice(), score));
-    }
+  playerPickIcon.setAttribute("class", pickIcon(playerSelection));
+  computerPickIcon.setAttribute("class", pickIcon(computerSelection));
 
-    if (score.player > score.computer) {
-        resultStatement = "You Won";
-    } else if (score.player < score.computer) {
-        resultStatement = "You Lose";
+  playerScoreValue.textContent = playerScore;
+  computerScoreValue.textContent = computerScore;
+}
+
+function playRound(playerSelection, computerSelection) {
+  let picksResultValue = document.querySelector("#picksResultValue");
+
+  switch (computerSelection) {
+    case "Rock":
+      if (playerSelection === computerSelection) {
+        playerScore++;
+        computerScore++;
+        showWinMsg(picksResultValue, "tie");
+      } else if (playerSelection === "Paper") {
+        playerScore++;
+        showWinMsg(picksResultValue, "player");
+      } else {
+        computerScore++;
+        showWinMsg(picksResultValue, "computer");
+      }
+      break;
+    case "Paper":
+      if (playerSelection === computerSelection) {
+        showWinMsg(picksResultValue, "tie");
+        playerScore++;
+        computerScore++;
+      } else if (playerSelection === "Scissors") {
+        playerScore++;
+        showWinMsg(picksResultValue, "player");
+      } else {
+        computerScore++;
+        showWinMsg(picksResultValue, "computer");
+      }
+      break;
+    case "Scissors":
+      if (playerSelection === computerSelection) {
+        showWinMsg(picksResultValue, "tie");
+        playerScore++;
+        computerScore++;
+      } else if (playerSelection === "Rock") {
+        playerScore++;
+        showWinMsg(picksResultValue, "player");
+      } else {
+        computerScore++;
+        showWinMsg(picksResultValue, "computer");
+      }
+      break;
+    default:
+      return "Invalid selection!";
+  }
+}
+
+function announceResults() {
+  let pickButtons = document.querySelectorAll(".pickButton");
+  let resetButton = document.querySelector(".resetButton");
+  let result = document.querySelector(".result");
+
+  if (isGameOver()) {
+    let resultValue = document.createElement("p");
+    resultValue.classList.add("gameResultValue");
+
+    pickButtons.forEach((element) => {
+      element.style.display = "none";
+    });
+    resetButton.style.display = "block";
+    result.style.display = "block";
+
+    if (playerScore > computerScore) {
+      showWinMsg(resultValue, "player");
+    } else if (playerScore < computerScore) {
+      showWinMsg(resultValue, "computer");
     } else {
-        resultStatement = "It's a Tie";
+      showWinMsg(resultValue, "none");
     }
-
-    console.log(
-        `The final score is: ${score.player}-${score.computer} - ${resultStatement}!`
-    );
+    result.appendChild(resultValue);
+  }
 }
 
-game();
+function game(e) {
+  if (isGameOver()) {
+    location.reload();
+    return false;
+  }
+
+  const playerSelection = e.target.id;
+  const computerSelection = getComputerChoice();
+
+  playRound(playerSelection, computerSelection);
+  showResults(playerSelection, computerSelection);
+  announceResults();
+}
+
+const play = new (function() {
+  let playerChoice = document.querySelector(".buttons");
+  playerChoice.addEventListener("click", game);
+})();
